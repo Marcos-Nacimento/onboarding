74fd43d26ea86fb6af9766c3bc29cfcd6db2e3d1
Marcos-Nacimento <marcosftp1@gmail.com>
2022-05-30 11:08:47 -0300

    added font custom in title order

--- src/components/order.tsx
+++ src/components/order.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Order: React.FC = () => {
     return (
         <div className="border border-y-0 border-l-0 border-slate-800 w-[19rem]">
-            <div className="h-10 border border-x-0 border-t-0 border-slate-800">
+            <div className="h-10 border border-x-0 border-t-0 border-slate-800 font-body select-none">
                 <p className="text-white text-sm p-2">Orders</p>
             </div>
         </div>
