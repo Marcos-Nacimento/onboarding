5f0457106c2d913f88c0a1edcec0f43558856b28
Marcos-Nacimento <marcosftp1@gmail.com>
2022-05-30 12:48:07 -0300

    refactoring

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,12 +13,12 @@ const Header: React.FC = () => {
     return (
         <div className="flex justify-end h-10 border border-x-0 border-t-0 py-3 px-2 border-slate-800">
             <div className="flex space-x-4 mr-4">
-                <CubeIcon className="text-gray-300 h-5 cursor-pointer hover:text-red-500"/>
-                <UserAddIcon className="text-gray-300 h-5 cursor-pointer hover:text-red-500"/>
-                <BellIcon className="text-gray-300 h-5 cursor-pointer hover:text-red-500"/>
+                <CubeIcon className="text-gray-300 h-5 cursor-pointer hover:text-green-400"/>
+                <UserAddIcon className="text-gray-300 h-5 cursor-pointer hover:text-green-400"/>
+                <BellIcon className="text-gray-300 h-5 cursor-pointer hover:text-green-400"/>
                 <UserCircleIcon
                     onClick={() => navigate("/account")}
-                    className="text-gray-300 h-5 cursor-pointer hover:text-red-500"
+                    className="text-gray-300 h-5 cursor-pointer hover:text-green-400"
                 />
             </div>
         </div>
