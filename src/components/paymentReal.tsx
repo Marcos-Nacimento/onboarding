66d4589d91d296072d50b7642f630f643e5aadd3
Marcos-Nacimento <marcosftp1@gmail.com>
2022-06-01 16:54:19 -0300

    component to show payments sended

---
+++ src/components/paymentReal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PaymentReal: React.FC = () => {
+    return (
+        <div className="h-[20rem] w-full max-w-sm bg-gradient-to-r from-emerald-600 to-emerald-900 rounded-md shadow-xl">
+            
+        </div>
+    );
+};
+
+export default PaymentReal;
