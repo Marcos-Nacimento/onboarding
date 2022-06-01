c1f7983bb290d9301dd1264339ecc0ffa3e279e7
Marcos-Nacimento <marcosftp1@gmail.com>
2022-06-01 16:52:13 -0300

    component to shows activities in real time

---
+++ src/components/activities.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Activities: React.FC = () => {
+    return (
+        <div className="h-[30rem] w-full bg-gradient-to-r from-emerald-600 to-emerald-900 rounded-md shadow-xl">
+            
+        </div>
+    );
+};
+
+export default Activities;
