8c9959dab7bd376635a1176a8585aecbba8444da
Marcos-Nacimento <marcosftp1@gmail.com>
2022-06-04 12:07:36 -0300

    refactoring and removindo components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,9 @@
 import React from "react";
-import {
-    UserAddIcon,
-    BellIcon,
-    UserCircleIcon
-} from "@heroicons/react/solid";
-import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
-    let navigate = useNavigate();
-
     return (
         <div className="flex justify-end h-10 border border-x-0 border-t-0 py-3 px-2 border-zinc-800">
-            <div className="flex space-x-4 mr-4">
-                <UserAddIcon className="text-gray-300 h-5 cursor-pointer hover:text-green-400"/>
-                <BellIcon className="text-gray-300 h-5 cursor-pointer hover:text-green-400"/>
-                <UserCircleIcon
-                    onClick={() => navigate("/account")}
-                    className="text-gray-300 h-5 cursor-pointer hover:text-green-400"
-                />
-            </div>
+            
         </div>
     );
 };
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import SideBarSmall from "../components/sideBarSmall";
-import Order from "../components/order";
+import SideBar from "../components/sideBar";
 import Header from "../components/header";
 
 import { Outlet } from "react-router-dom";
@@ -8,8 +7,7 @@ import { Outlet } from "react-router-dom";
 const Layout: React.FC = () => {
     return (
         <div className='flex h-[100vh] bg-[#111]'>
-            <SideBarSmall />
-            <Order />
+            <SideBar />
             <div className="w-full">
                 <Header />
                 <Outlet />
